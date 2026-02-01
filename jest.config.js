edb003b5425feefc3553665bc25ab2be324032a4
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2026-02-01 08:37:14 -0500

    Fix AWS dependency issues

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,13 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/_tests_/**/*.test.js'],
   collectCoverageFrom: ['src/**/*.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(@actions|@octokit|before-after-hook|universal-user-agent)/)'
-  ],
+  transformIgnorePatterns: ['node_modules/(?!(@actions|@octokit|before-after-hook|universal-user-agent)/)'],
   transform: {
-    '^.+\\.js$': ['babel-jest', { 
-      presets: [['@babel/preset-env', { targets: { node: 'current' } }]] 
-    }]
+    '^.+\\.js$': [
+      'babel-jest',
+      {
+        presets: [['@babel/preset-env', {targets: {node: 'current'}}]]
+      }
+    ]
   }
 };
